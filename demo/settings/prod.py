c62912276daa4a40eb449287c622e5362802db67
ssibuyi <saksman3@gmail.com>
2021-08-17 11:13:54 +0200

    initial commit

---
+++ demo/settings/prod.py
@@ -0,0 +1,33 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Django Boilerplate
+## This boilerplate is designed to allow the flexibility between working in production and also in development.
+## Run ```pip install -r requirements.txt``` to install required python packages.
+### - Change the environment inside manage.py in the line `os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings.dev')` 
+## A command for changing the project name to your preferred name also exists
+### -python
+## Configurations can be done inside the .env file here you can define variables that should be kept secret 
+### - Secret keys, Passwords or DB access details.
